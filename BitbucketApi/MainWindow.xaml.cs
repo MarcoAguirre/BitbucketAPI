8430ec01b14da21d0104b2fc652d939d4825d1ba
MarcoAguirre <viniciosoria16@gmail.com>
2020-03-11 13:20:07 -0500

    API Almost finished

--- BitbucketApi/MainWindow.xaml.cs
+++ BitbucketApi/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Net.Http;
+using System.Net;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BitbucketApi
@@ -25,10 +27,29 @@ namespace BitbucketApi
             return strContentResponse;
         }
 
+        public string GETJSON(string strURL)
+        {
+            string strJSON = "";
+
+            WebRequest webRequest = WebRequest.Create(strURL);
+            webRequest.Method = "GET";
+            HttpWebResponse webResponse = null;
+            webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            Stream stream = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            strJSON = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            return strJSON;
+        }
+
         private void btnGet_Click(object sender, RoutedEventArgs e)
         {
-            var response = Get();
-            MessageBox.Show(Convert.ToString( response));
+            /*var response = Get();
+            MessageBox.Show(Convert.ToString( response));*/
+
+            MessageBox.Show(GETJSON("http://www.google.com.ec"));
         }
     }
 }
