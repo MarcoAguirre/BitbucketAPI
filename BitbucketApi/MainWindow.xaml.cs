946197381510c5e6f7cf90cbecec5788f1a1ea89
MarcoAguirre <viniciosoria16@gmail.com>
2020-03-11 09:16:10 -0500

    First commit for the solution

---
+++ BitbucketApi/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BitbucketApi
+{
+    public partial class MainWindow : Window
+    {
+        private HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dynamic response = Get();
+            MessageBox.Show(response.ToString());
+        }
+
+        public async Task<dynamic> Get()
+        {
+            HttpResponseMessage responseJson = await client.GetAsync("http://www.etnassoft.com/api/v1/get/?id=589&callback=?");
+            HttpContent content = responseJson.Content;
+
+
+            return content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ BitbucketApi/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitbucketApi
+{
+    public class Repository
+    {
+        string m_strRepositoryName = "";
+        string m_strRepositoryCommentary = "";
+
+        public string StrRepositoryName 
+        { 
+            get => m_strRepositoryName; 
+            set => m_strRepositoryName = value; 
+        }
+
+        public string StrRepositoryCommentary 
+        { 
+            get => m_strRepositoryCommentary; 
+            set => m_strRepositoryCommentary = value; 
+        }
+    }
+}
