84a8bdb1bd896513aad396d0f1c85af0efbf719d
MarcoAguirre <viniciosoria16@gmail.com>
2020-03-11 15:53:09 -0500

    GET Success

--- BitbucketApi/MainWindow.xaml.cs
+++ BitbucketApi/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net;
 using System.IO;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace BitbucketApi
 {
@@ -44,12 +45,28 @@ namespace BitbucketApi
             return strJSON;
         }
 
+        public async Task<HttpResponseMessage> GETJSONRepositories(string strURL, string strUser, string strPassword)
+        {
+            //string strRepoData = "";
+
+            HttpRequestMessage httpRequest = new HttpRequestMessage(new HttpMethod("GET"), strURL + strUser);
+            string vAuthorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(strUser + ":" + strPassword));
+            httpRequest.Headers.TryAddWithoutValidation("Authorization", $"Basic {vAuthorization}");
+
+            HttpResponseMessage response = await client.SendAsync(httpRequest);
+
+            return response;
+        }
+
         private void btnGet_Click(object sender, RoutedEventArgs e)
         {
             /*var response = Get();
             MessageBox.Show(Convert.ToString( response));*/
 
-            MessageBox.Show(GETJSON("http://www.google.com.ec"));
+            //MessageBox.Show(GETJSON("http://www.google.com.ec"));
+
+            MessageBox.Show(GETJSONRepositories("https://api.bitbucket.org/2.0/repositories/", txtUserName.Text, 
+                pbPassword.Password).ToString());
         }
     }
 }
